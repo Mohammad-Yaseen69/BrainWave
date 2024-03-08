066fa94b18673fea3f2b9eda0167f95f21567816
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-03-08 21:11:24 +0500

    HamBurger Menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,33 +1,71 @@
-import React from 'react'
 import { brainwave } from '../../assets'
 import { Button } from '../'
-import { navigation as navLinks } from '../../constant'
+import { navigation } from '../../constant'
 import { useLocation } from 'react-router-dom'
+import { HamburgerMenu } from '../Design/Header'
+import MenuSvg from '../../assets/svg/MenuSvg'
+import { useState } from 'react'
 
 const Header = () => {
 
+    const [open, setOpen] = useState(true)
+
+    function handleMenu() {
+        setOpen(!open)
+    }
+
+    function handleClick() {
+        setOpen(false)
+    }
 
     return (
-        <div className='fixed py-[1.30rem] bg-n-8/90 backdrop-blur border-b border-n-6 top-0 left-0 flex z-20 items-center  justify-between w-full'>
-            <div className='px-5 lg:px-8'>
-                <a href="#">
-                    <img src={brainwave} alt="" width={190} height={40} />
+        <div
+            className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${open ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+                }`}
+        >
+            <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+                <a className="block w-[12rem] xl:mr-8" href="#hero">
+                    <img src={brainwave} width={190} height={40} alt="Brainwave" />
                 </a>
-            </div>
-            <nav className='hidden flex-1 lg:flex '>
-                <div className=' flex items-center  w-full  justify-evenly'>
-                    {navLinks.map((link, index) => (
-                        <a href={link.url} key={index} className={`text-n-3/90 font-code text-sm font-bold ${link.onlyMobile && "lg:hidden"} lg:text-xs hover:text-n-1`}>{link.title.toUpperCase()}</a>
-                    ))}
-                </div>
 
-                <div className='flex px-5 lg:px-8 items-center justify-center gap-9'>
-                    <a href="" className='text-n-3/90 whitespace-nowrap font-code text-sm font-extrabold lg:text-xs hover:text-n-1'>NEW ACCOUNT</a>
-                    <Button className="whitespace-nowrap hidden lg:flex">
-                        Sign In
-                    </Button>
-                </div>
-            </nav>
+                <nav
+                    className={`${open ? "flex" : "hidden"
+                        } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+                >
+                    <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+                        {navigation.map((item) => (
+                            <a
+                                key={item.id}
+                                href={item.url}
+                                onClick={handleClick}
+                                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? "lg:hidden" : ""} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                            >
+                                {item.title}
+                            </a>
+                        ))}
+                    </div>
+
+                    <HamburgerMenu />
+                </nav>
+
+                <a
+                    href="#signup"
+                    className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+                >
+                    New account
+                </a>
+                <Button className="hidden lg:flex" href="#login">
+                    Sign in
+                </Button>
+
+                <Button
+                    className="ml-auto lg:hidden"
+                    px="px-3"
+                    onClick={handleMenu}
+                >
+                    <MenuSvg openNavigation={open} />
+                </Button>
+            </div>
         </div>
     )
 }
