7db112b942e426e690196e08f6662253f3282b69
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-03-14 15:26:25 +0500

    build - hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { curve, heroBackground, robot } from '../../assets/index'
+import { useRef } from 'react'
 import { Button, Section } from '../'
+import { curve, heroBackground, robot } from '../../assets/index'
+import { BackgroundCircles, BottomLine, Gradient } from '../Design/Hero'
+import { heroIcons, } from '../../constant'
+import { ScrollParallax } from 'react-just-parallax'
 
 const Hero = () => {
+    const parallaxRef = useRef(null)
+
     return (
         <Section
-            className="pt-[12rem] -my-[5.25]"
+            className="-my-[5.25]"
+            paddings="pt-[12rem] "
             crosses
             crossesOffset="lg:translate-y-[5.28rem]"
             id="hero"
         >
 
-            <div className='container relative '>
+            <div ref={parallaxRef} className='container relative '>
                 <div className='text-center mb-[4rem] lg:pt-[2rem] sm:w-full max-w-[55rem]  relative mx-auto'>
                     <h1 className='h1 mb-6'>
                         Explore the Possibilities of AI Chatting
@@ -28,19 +34,41 @@ const Hero = () => {
                     </Button>
                 </div>
 
-                <div className='relative mx-auto xl:mb-24 max-w-[20rem] sm:max-w-5xl'>
+                <div className=' relative mx-auto xl:mb-24 max-w-[20rem] sm:max-w-5xl'>
                     <div className='relative rounded-2xl bg-conic-gradient p-0.5'>
                         <div className='relative bg-n-8 rounded-[1rem]'>
                             <div className='bg-n-10 h-[1.5rem] rounded-t-[0.9rem]' />
                             <div className='aspect-[33/40] md:aspect-[688/490] lg:aspect-[1024/490] overflow-hidden  rounded-b-[0.9rem]'>
                                 <img src={robot}
-                                    className='w-full scale-[1.7] lg:-translate-y-[23%]  md:scale-100  md:-translate-y-[10%] translate-y-[8%]'
+                                    className= 'z-3 w-full scale-[1.7] lg:-translate-y-[23%]  md:scale-100  md:-translate-y-[10%] translate-y-[8%]'
                                     width={1024}
                                     height={490}
                                     alt="robot"
                                 />
                             </div>
+                            <ScrollParallax isAbsolutelyPositioned >
+                                <ul className='hidden xl:flex absolute -left-[5.5rem] 
+                                bottom-[7.8rem] backdrop-blur p-1 rounded-2xl bg-n-9/40 border-n-1/10 border'>
+                                    {heroIcons.map((icon, idx) => (
+                                        <li key={idx} className='p-5'>
+                                            <img src={icon} alt="" />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </ScrollParallax>
+
+                            <ScrollParallax isAbsolutelyPositioned>
+                                <ul className='hidden xl:flex absolute -right-[5.5rem] 
+                                bottom-[13rem] backdrop-blur p-1 rounded-2xl bg-n-9/40 border-n-1/10 border'>
+                                    {heroIcons.map((icon, idx) => (
+                                        <li key={idx} className='p-5'>
+                                            <img src={icon} alt="" />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </ScrollParallax>
                         </div>
+                        <Gradient />
                     </div>
                     <div className="absolute -z-3 -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
                         <img
@@ -51,9 +79,11 @@ const Hero = () => {
                             alt="hero"
                         />
                     </div>
+                    <BackgroundCircles parallaxRef={parallaxRef} />
 
                 </div>
             </div>
+            <BottomLine />
         </Section>
     )
 }
